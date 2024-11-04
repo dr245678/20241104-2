e4b2c02081e30e511f39a69cc46934b57a4e4507
V0-初始專案 <juiklop3579@gmail.com>
2024-11-04 15:21:20 +0800

    v2_姓名輸入

--- app/src/main/java/com/example/a20241104_2/MainActivity.kt
+++ app/src/main/java/com/example/a20241104_2/MainActivity.kt
@@ -51,6 +51,23 @@ class MainActivity : ComponentActivity() {
 @Composable
 
 fun Birth(m: Modifier) {
+    var userName by remember { mutableStateOf("mintuz")}
+
+    Column {
+        TextField(
+            value = userName,
+            onValueChange = { newText ->
+                userName = newText
+            },
+            modifier = m,
+                    label = { Text("姓名") },
+            placeholder = { Text("請輸入您的姓名") }
+
+        )
+
+        Text("您輸入的姓名是：$userName")
+
+    }
 
 }
 }
