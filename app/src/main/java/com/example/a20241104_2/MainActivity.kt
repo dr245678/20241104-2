41e6eec4a920e1eacde893ae3098c642dba47f86
V0-初始專案 <juiklop3579@gmail.com>
2024-11-04 15:27:52 +0800

    v3_出生體重輸

--- app/src/main/java/com/example/a20241104_2/MainActivity.kt
+++ app/src/main/java/com/example/a20241104_2/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.Button
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.text.input.KeyboardType
 
 
 class MainActivity : ComponentActivity() {
@@ -52,6 +53,8 @@ class MainActivity : ComponentActivity() {
 
 fun Birth(m: Modifier) {
     var userName by remember { mutableStateOf("mintuz")}
+    var userWeight by remember { mutableStateOf(3800)}
+
 
     Column {
         TextField(
@@ -65,7 +68,21 @@ fun Birth(m: Modifier) {
 
         )
 
-        Text("您輸入的姓名是：$userName")
+        TextField(
+            value = userWeight.toString(),
+            onValueChange = { newText ->
+                if (newText == ""){
+                    userWeight = 0
+                }
+                else{userWeight = newText.toInt()}
+            },
+            label = { Text("出生體重") },
+            keyboardOptions = KeyboardOptions
+                (keyboardType = KeyboardType.Number)
+        )
+
+
+        Text("您輸入的姓名是：$userName\n出生體重為：$userWeight 公克")
 
     }
 
